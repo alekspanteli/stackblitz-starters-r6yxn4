8c35cbc717aa1726d2a04fb75bf2d2646329e650
alekspanteli <alekspanteli@rambler.ru>
2024-01-10 17:34:51 +0200

    tewst2

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hey!!!!!!!!!!!!!
+    <h1 className="text-3xl font-bold underline text-red-500">
+      Hey!!!!!!!!!!
     </h1>
   )
 }
